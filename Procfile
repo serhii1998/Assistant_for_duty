80afed29a62ec78d96d93e3a8f09173b715d8a96
EvgenRomanov98 <evrmo98@gmail.com>
2019-01-17 13:42:21 +0200

    destroy

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -jar target/assistantAttendant-1.0.jar
--- src/main/java/com/attendant/threads/UpTimeThread.java
+++ src/main/java/com/attendant/threads/UpTimeThread.java
@@ -10,28 +10,28 @@ public class UpTimeThread implements Runnable {
 
     @Override
     public void run() {
-        while(true){
-            System.out.println("in UpTimeThread while");
-
-            StringBuilder stringBuilder = new StringBuilder();
-
-            try(InputStream inputStream = new URL("https://assistant-attendant.herokuapp.com/").openStream();
-                    BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream)){
-
-                int symbol;
-                while ((symbol = bufferedInputStream.read()) != -1){
-                    stringBuilder.append((char)symbol);
-                }
-
-                System.out.println(stringBuilder.toString());
-
-                System.out.println("in UpTimeThread while sleep 300000");
-
-                sleep(300000);//пробуждение через каждые 5 мин
-
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-        }
+//        while(true){
+//            System.out.println("in UpTimeThread while");
+//
+//            StringBuilder stringBuilder = new StringBuilder();
+//
+//            try(InputStream inputStream = new URL("https://assistant-attendant.herokuapp.com/").openStream();
+//                    BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream)){
+//
+//                int symbol;
+//                while ((symbol = bufferedInputStream.read()) != -1){
+//                    stringBuilder.append((char)symbol);
+//                }
+//
+//                System.out.println(stringBuilder.toString());
+//
+//                System.out.println("in UpTimeThread while sleep 300000");
+//
+//                sleep(300000);//пробуждение через каждые 5 мин
+//
+//            }catch (Exception e){
+//                e.printStackTrace();
+//            }
+//        }
     }
 }
