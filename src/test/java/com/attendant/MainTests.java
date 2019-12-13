cd5df37f043c3aee286dbee39a40e564c4ad394f
dn070998rei1 <evgenij.romanov@privatbank.ua>
2019-12-13 17:04:54 +0200

    test add

---
+++ src/test/java/com/attendant/MainTests.java
@@ -0,0 +1,13 @@
+package com.attendant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistantDutyBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
